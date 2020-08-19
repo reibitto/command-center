86b725d933356aea8c4133152e09af98ed04f137
duester <maxc@gmx.de>
2020-08-19 13:55:15 +0300

    Calculator enhancements (#87)
    * Calculator enhancements:
    - double => BigDecimal
    - added functions
    - integer values formatted without decimal separator
    - configurable parsing/displaying parameters
    
    * - moved HelpType and Parameters inside `object CalculatorCommand`
    - changed argument of DecimalFormat.format to java.math.BigDecimal

--- core/src/main/scala/commandcenter/command/CalculatorCommand.scala
+++ core/src/main/scala/commandcenter/command/CalculatorCommand.scala
@@ -1,29 +1,130 @@
 package commandcenter.command
 
+import java.text.{ DecimalFormat, DecimalFormatSymbols }
+import java.util.Locale
+
+import cats.data.Validated
+import com.monovore.decline
+import com.monovore.decline.Opts
 import com.typesafe.config.Config
 import commandcenter.CCRuntime.Env
+import commandcenter.command.CalculatorCommand.{ FunctionsList, Parameters, ParametersList }
 import commandcenter.command.util.CalculatorUtil
 import commandcenter.tools
+import commandcenter.view.DefaultView
+import io.circe.Decoder
 import zio.{ TaskManaged, ZIO, ZManaged }
 
-final case class CalculatorCommand() extends Command[Double] {
+final case class CalculatorCommand(parameters: Parameters) extends Command[BigDecimal] {
   val commandType: CommandType = CommandType.CalculatorCommand
 
-  val commandNames: List[String] = List.empty
+  val commandNames: List[String] = List("calculator")
 
   val title: String = "Calculator"
 
-  def preview(searchInput: SearchInput): ZIO[Env, CommandError, List[PreviewResult[Double]]] =
+  private val helpTypeOpt = Opts.argument[String]("functions or parameters").mapValidated {
+    case arg if arg.matches("functions?")  => Validated.valid(FunctionsList)
+    case arg if arg.matches("parameters?") => Validated.valid(ParametersList)
+    case arg                               => Validated.invalidNel(s"$arg is not valid: should be 'functions' or 'parameters'.")
+  }
+
+  def preview(searchInput: SearchInput): ZIO[Env, CommandError, List[PreviewResult[BigDecimal]]] =
+    previewHelp(searchInput) <> previewEvaluation(searchInput)
+
+  private def previewHelp(searchInput: SearchInput): ZIO[Env, CommandError, List[PreviewResult[BigDecimal]]] =
     for {
-      evaluatedValue <-
-        ZIO.fromOption(CalculatorUtil.evaluate(searchInput.input)).orElseFail(CommandError.NotApplicable)
+      input                <- ZIO.fromOption(searchInput.asArgs).orElseFail(CommandError.NotApplicable)
+      parsed                = decline.Command(title, title)(helpTypeOpt).parse(input.args)
+      (helpTitle, message) <- ZIO
+                                .fromEither(parsed)
+                                .fold(
+                                  help => (title, HelpMessage.formatted(help)),
+                                  helpType => (helpType.title, CalculatorCommand.helpMessageByType(helpType))
+                                )
+    } yield List(
+      Preview(BigDecimal(0.0)).score(Scores.high(searchInput.context)).view(DefaultView(helpTitle, message))
+    )
+
+  private def previewEvaluation(searchInput: SearchInput): ZIO[Env, CommandError, List[PreviewResult[BigDecimal]]] =
+    for {
+      evaluatedValue <- ZIO
+                          .fromOption(new CalculatorUtil(parameters).evaluate(searchInput.input))
+                          .orElseFail(CommandError.NotApplicable)
     } yield List(
       Preview(evaluatedValue)
-        .onRun(tools.setClipboard(evaluatedValue.toString))
+        .render(parameters.format)
+        .onRun(tools.setClipboard(parameters.format(evaluatedValue)))
         .score(Scores.high(searchInput.context))
     )
 }
 
 object CalculatorCommand extends CommandPlugin[CalculatorCommand] {
-  def make(config: Config): TaskManaged[CalculatorCommand] = ZManaged.succeed(CalculatorCommand())
+  def make(config: Config): TaskManaged[CalculatorCommand] = ZManaged.fromEither(config.as[CalculatorCommand])
+
+  implicit val decoder: Decoder[CalculatorCommand] = Decoder.instance { c =>
+    val decimalFormat        = new DecimalFormat()
+    val decimalFormatSymbols = decimalFormat.getDecimalFormatSymbols
+    for {
+      decimalSeparator      <- c.get[Option[Char]]("decimalSeparator")
+      groupingSeparator     <- c.get[Option[Char]]("groupingSeparator")
+      parameterSeparator    <- c.get[Option[Char]]("parameterSeparator")
+      groupingSize          <- c.get[Option[Int]]("groupingSize")
+      groupingUsed          <- c.get[Option[Boolean]]("groupingUsed")
+      maximumFractionDigits <- c.get[Option[Int]]("maximumFractionDigits")
+    } yield CalculatorCommand(
+      Parameters(
+        decimalSeparator.getOrElse(decimalFormatSymbols.getDecimalSeparator),
+        groupingSeparator.getOrElse(decimalFormatSymbols.getGroupingSeparator),
+        parameterSeparator.getOrElse(';'),
+        groupingSize.getOrElse(decimalFormat.getGroupingSize),
+        groupingUsed.getOrElse(decimalFormat.isGroupingUsed),
+        maximumFractionDigits.getOrElse(decimalFormat.getMaximumFractionDigits)
+      )
+    )
+  }
+
+  def helpMessageByType(helpType: HelpType): fansi.Str =
+    helpType match {
+      case FunctionsList  => CalculatorUtil.helpMessageFunctionsList
+      case ParametersList => CalculatorUtil.helpMessageParametersList
+    }
+
+  sealed trait HelpType extends Product {
+    val title: String
+  }
+
+  case object FunctionsList extends HelpType {
+    override val title: String = "List of all operators/functions"
+  }
+
+  case object ParametersList extends HelpType {
+    override val title: String = "List of all configuration parameters"
+  }
+
+  final case class Parameters(
+    decimalSeparator: Char,
+    groupingSeparator: Char,
+    parameterSeparator: Char,
+    groupingSize: Int,
+    groupingUsed: Boolean,
+    maximumFractionDigits: Int
+  ) {
+    def decimalFormat: DecimalFormat =
+      new DecimalFormat() {
+        setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.getDefault) {
+          setDecimalSeparator(decimalSeparator)
+          setGroupingSeparator(groupingSeparator)
+        })
+        setGroupingSize(groupingSize)
+        setGroupingUsed(groupingUsed)
+        setMaximumFractionDigits(maximumFractionDigits)
+        setParseBigDecimal(true)
+      }
+
+    def format(value: BigDecimal): String = {
+      val format = decimalFormat
+      if (value.isWhole) format.setMaximumFractionDigits(0)
+      format.format(value.bigDecimal)
+    }
+  }
 }
