7acd5f086e0c1f8e7db50a22da2fdc5f41638642
reibitto <reibitto@users.noreply.github.com>
2020-08-28 00:39:45 +0900

    Begin adding opts for cli-client Main

--- cli-client/src/main/scala/commandcenter/cli/Main.scala
+++ cli-client/src/main/scala/commandcenter/cli/Main.scala
@@ -7,11 +7,16 @@ import commandcenter.shortcuts.Shortcuts
 import commandcenter.ui.{ CliTerminal, EventResult }
 import commandcenter.{ CCApp, CCConfig }
 import zio._
+import zio.console._
 import zio.stream.ZStream
 
 object Main extends CCApp {
-  def run(args: List[String]): URIO[Env, ExitCode] =
-    (for {
+  // TODO: Get version from build info
+  def printVersion: URManaged[Console, ExitCode] =
+    putStrLn("Command Center CLI v0.0.1").exitCode.toManaged_
+
+  def uiLoop: RManaged[Env, ExitCode] =
+    for {
       config   <- CCConfig.load
       terminal <- CliTerminal.createNative(config)
       exitCode <- (for {
@@ -36,10 +41,17 @@ object Main extends CCApp {
                                case EventResult.Success            => true
                              }
                     } yield ()).exitCode.toManaged_
-    } yield exitCode).useNow.catchAll { t =>
+    } yield exitCode
+
+  def run(args: List[String]): URIO[Env, ExitCode] = {
+    // TODO: Add proper parsing with Decline. Add `--help`, `--verison`, `--numberic-version`, etc.
+    val main = if (args.isEmpty) uiLoop else printVersion
+
+    main.useNow.catchAll { t =>
       t.printStackTrace()
       UIO(ExitCode.failure)
     }
+  }
 
   val shortcutsLayer: ULayer[Shortcuts] = Shortcuts.unsupported
 }
