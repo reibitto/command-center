e6f3c94f36c331fb6bbcd2cb3ae43f94a1840c3a
Scala Steward <me@scala-steward.org>
2020-09-01 19:32:11 +0200

    Update client:circe, client:core, ... to 2.2.7

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object Build {
   object Version {
     val zio   = "1.0.1"
     val circe = "0.13.0"
-    val sttp  = "2.2.6"
+    val sttp  = "2.2.7"
     val graal = "20.2.0"
 
     // If you set this to None you can test with your locally installed version of Graal. Otherwise it will run in Docker
