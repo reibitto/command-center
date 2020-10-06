c00f4ec74e7fefb348fcf609aaf17f237881c4ac
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-10-06 13:49:41 +0200

    Update zio, zio-streams, zio-test, ... to 1.0.2 (#122)

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object Build {
   val CommandCenterVersion = "0.0.1"
 
   object Version {
-    val zio   = "1.0.1"
+    val zio   = "1.0.2"
     val circe = "0.13.0"
     val sttp  = "2.2.9"
     val graal = "20.2.0"
