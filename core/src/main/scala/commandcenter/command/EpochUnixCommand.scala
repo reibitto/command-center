f75971dda30bb58000f73b0b8dbea14e0dac62a4
reibitto <reibitto@users.noreply.github.com>
2020-10-11 23:46:16 +0900

    Decode epoch time support (#129)
    * Decode epoch time
    
    * Tweak scoring and fix tests

--- core/src/main/scala/commandcenter/command/EpochUnixCommand.scala
+++ core/src/main/scala/commandcenter/command/EpochUnixCommand.scala
@@ -1,24 +1,48 @@
 package commandcenter.command
 
+import java.time.{ Instant, ZoneId }
+import java.time.format.{ DateTimeFormatter, FormatStyle }
 import java.util.concurrent.TimeUnit
 
 import com.typesafe.config.Config
 import commandcenter.CCRuntime.Env
 import commandcenter.tools
-import zio.{ clock, TaskManaged, ZIO, ZManaged }
+import zio.{ clock, Task, TaskManaged, ZIO, ZManaged }
 
-final case class EpochUnixCommand(commandNames: List[String]) extends Command[Long] {
+final case class EpochUnixCommand(commandNames: List[String]) extends Command[String] {
   val commandType: CommandType = CommandType.EpochUnixCommand
   val title: String            = "Epoch (Unix time)"
 
-  def preview(searchInput: SearchInput): ZIO[Env, CommandError, List[PreviewResult[Long]]] =
+  def preview(searchInput: SearchInput): ZIO[Env, CommandError, List[PreviewResult[String]]] =
     for {
-      input     <- ZIO.fromOption(searchInput.asKeyword).orElseFail(CommandError.NotApplicable)
-      epochTime <- clock.currentTime(TimeUnit.SECONDS)
+      input           <- ZIO.fromOption(searchInput.asPrefixed).orElseFail(CommandError.NotApplicable)
+      (output, score) <- if (input.rest.trim.isEmpty) {
+                           (clock.currentTime(TimeUnit.SECONDS).map(time => (time.toString, Scores.high)))
+                         } else {
+                           input.rest.toLongOption match {
+                             case Some(seconds) =>
+                               Task {
+                                 val formatted = Instant
+                                   .ofEpochSecond(seconds)
+                                   .atZone(ZoneId.systemDefault())
+                                   .format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM))
+
+                                 val score = if (seconds < 100000000000L) {
+                                   Scores.high(input.context)
+                                 } else {
+                                   Scores.high(input.context) * 0.9
+                                 }
+
+                                 (formatted, score)
+                               }.mapError(CommandError.UnexpectedException)
+
+                             case None => ZIO.fail(CommandError.NotApplicable)
+                           }
+                         }
     } yield List(
-      Preview(epochTime)
-        .score(Scores.high(input.context))
-        .onRun(tools.setClipboard(epochTime.toString))
+      Preview(output)
+        .score(score)
+        .onRun(tools.setClipboard(output))
     )
 }
 
