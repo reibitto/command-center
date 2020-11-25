3c492a879c6f9b8827a74ad774a0ec0da357d07c
reibitto <reibitto@users.noreply.github.com>
2020-11-25 19:04:49 +0900

    Improve Maven search (#142)

---
+++ core/src/main/scala/commandcenter/util/Orderings.scala
@@ -0,0 +1,11 @@
+package commandcenter.util
+
+import java.time.LocalDate
+
+import scala.math.Ordering
+
+object Orderings {
+  object LocalDateOrdering extends Ordering[LocalDate] {
+    def compare(x: LocalDate, y: LocalDate): Int = x.compareTo(y)
+  }
+}
