125720ccfa2fe16402044fb94510c750d9e1d3db
Scala Steward <me@scala-steward.org>
2020-12-01 01:12:37 +0100

    Update kind-projector to 0.11.2

--- project/Plugins.scala
+++ project/Plugins.scala
@@ -3,6 +3,6 @@ import sbt._
 object Plugins {
   lazy val BaseCompilerPlugins = Seq(
     compilerPlugin("com.olegpy"    %% "better-monadic-for" % "0.3.1"),
-    compilerPlugin("org.typelevel" %% "kind-projector"     % "0.11.1" cross CrossVersion.full)
+    compilerPlugin("org.typelevel" %% "kind-projector"     % "0.11.2" cross CrossVersion.full)
   )
 }
