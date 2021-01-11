be72a478c2ba6f1a2b289c6b75252eed6019f94f
reibitto <reibitto@users.noreply.github.com>
2021-01-12 03:51:18 +0900

    Clean up names

--- core/src/main/scala/commandcenter/util/Debouncer.scala
+++ core/src/main/scala/commandcenter/util/Debouncer.scala
@@ -5,7 +5,7 @@ import zio.clock.Clock
 import zio.duration.Duration
 
 final case class DebounceState[E, A](
-  fiber: Fiber[E, A],
+  running: Fiber[E, A],
   delay: Promise[Nothing, Unit],
   completion: Promise[Nothing, Unit],
   triggered: Boolean
@@ -44,14 +44,15 @@ object Debouncer {
       fn   = (f: ZIO[R, E, A]) =>
                ref.modify { state =>
                  for {
-                   promise     <- Promise.make[Nothing, Unit]
-                   completionP <- Promise.make[Nothing, Unit]
-                   _           <- ZIO.foreach_(state) { s =>
-                                    s.fiber.interrupt.unless(s.triggered)
-                                  }
-                   result      <- (promise.await.timeout(waitTime) *> f.tap(_ => completionP.succeed(()))).fork.map { fb =>
-                                    (fb, Some(DebounceState(fb, promise, completionP, triggered = false)))
-                                  }
+                   delayPromise      <- Promise.make[Nothing, Unit]
+                   completionPromise <- Promise.make[Nothing, Unit]
+                   _                 <- ZIO.foreach_(state) { s =>
+                                          s.running.interrupt.unless(s.triggered)
+                                        }
+                   result            <-
+                     (delayPromise.await.timeout(waitTime) *> f.tap(_ => completionPromise.succeed(()))).fork.map { fb =>
+                       (fb, Some(DebounceState(fb, delayPromise, completionPromise, triggered = false)))
+                     }
                  } yield result
                }
     } yield Debouncer(fn, ref)
