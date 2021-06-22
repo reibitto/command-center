cbead0fcd2f57cc08ee19680787002013cf5d8eb
reibitto <reibitto@users.noreply.github.com>
2021-06-22 23:51:14 +0900

    Improve WorldTimesCommand (#224)

--- core/src/main/scala/commandcenter/command/WorldTimesCommand.scala
+++ core/src/main/scala/commandcenter/command/WorldTimesCommand.scala
@@ -1,27 +1,41 @@
 package commandcenter.command
 
-import java.time.format.DateTimeFormatter
-import java.time.{ OffsetDateTime, ZoneId, ZonedDateTime }
-
 import com.typesafe.config.Config
 import commandcenter.CCRuntime.Env
-import commandcenter.view.View
-import commandcenter.{ CommandContext, TerminalType }
+import commandcenter.CommandContext
+import commandcenter.tools.Tools
+import commandcenter.view.Rendered
 import io.circe.Decoder
 import zio.{ TaskManaged, ZIO, ZManaged }
+
+import java.time.format.DateTimeFormatter
+import java.time.{ OffsetDateTime, ZoneId, ZonedDateTime }
 
 final case class WorldTimesCommand(commandNames: List[String], dateTimeFormat: String, zones: List[TimeZone])
     extends Command[Unit] {
   val commandType: CommandType = CommandType.WorldTimesCommand
   val title: String            = "World Times"
 
+  val displayFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern(dateTimeFormat)
+  val isoFormatter: DateTimeFormatter     = DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSSXXX")
+
   def preview(searchInput: SearchInput): ZIO[Env, CommandError, PreviewResults[Unit]] =
     for {
       input <- ZIO.fromOption(searchInput.asKeyword).orElseFail(CommandError.NotApplicable)
       now    = OffsetDateTime.now
       times  = zones.map(tz => WorldTimesResult(tz.zoneId, tz.displayName, now.atZoneSameInstant(tz.zoneId)))
-    } yield PreviewResults.one(
-      Preview.unit.view(WorldTimesResults(dateTimeFormat, times, input.context)).score(Scores.high(input.context))
+      _      = times.map(time => displayFormatter.format(time.dateTime))
+    } yield PreviewResults.fromIterable(
+      times.map { time =>
+        Preview.unit
+          .score(Scores.high(input.context))
+          .rendered(
+            Rendered.Ansi(
+              fansi.Color.Cyan(time.displayName) ++ fansi.Str(" ") ++ fansi.Str(displayFormatter.format(time.dateTime))
+            )
+          )
+          .onRun(Tools.setClipboard(isoFormatter.format(time.dateTime)))
+      }
     )
 }
 
@@ -36,32 +50,7 @@ object WorldTimesCommand extends CommandPlugin[WorldTimesCommand] {
     )
 }
 
-final case class WorldTimesResults(dateTimeFormat: String, results: List[WorldTimesResult], context: CommandContext) {
-  val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern(dateTimeFormat)
-}
-
-object WorldTimesResults {
-  implicit val displayable: View[WorldTimesResults] = View.ansi { results =>
-    // TODO: This is a temporary hack. Aligning should be handled automatically.
-    val align = results.context.terminal.terminalType match {
-      case TerminalType.Swing | TerminalType.Swt => "  "
-      case TerminalType.Cli | TerminalType.Test  => ""
-    }
-
-    intersperse(fansi.Str(s"\n$align")) {
-      results.results
-        .map(r => fansi.Color.Cyan(r.displayName) ++ fansi.Str(" ") ++ fansi.Str(results.formatter.format(r.dateTime)))
-    }.foldLeft(fansi.Str(""))(_ ++ _)
-  }
-
-  // TODO: Move to extension object
-  def intersperse[E](separator: E)(xs: List[E]): List[E] =
-    (separator, xs) match {
-      case (_, Nil)             => Nil
-      case (_, list @ _ :: Nil) => list
-      case (sep, y :: ys)       => y :: sep :: intersperse(sep)(ys)
-    }
-}
+final case class WorldTimesResults(dateTimeFormat: String, results: List[WorldTimesResult], context: CommandContext)
 
 final case class WorldTimesResult(zone: ZoneId, displayName: String, dateTime: ZonedDateTime)
 
---
+++ core/src/main/scala/commandcenter/util/CollectionExtensions.scala
@@ -0,0 +1,12 @@
+package commandcenter.util
+
+object CollectionExtensions {
+  implicit class ListExtension[A](val self: List[A]) extends AnyVal {
+    def intersperse(separator: A): List[A] =
+      (separator, self) match {
+        case (_, Nil)             => Nil
+        case (_, list @ _ :: Nil) => list
+        case (sep, y :: ys)       => y :: sep :: ys.intersperse(sep)
+      }
+  }
+}
