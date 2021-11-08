de467473839b2719c3435e8477448731a44d1120
Scala Steward <me@scala-steward.org>
2021-11-08 20:43:48 +0100

    Update jna, jna-platform to 5.10.0

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object Build {
     val sttp       = "3.3.15"
     val graal      = "20.2.0"
     val swt        = "3.117.0"
-    val jna        = "5.9.0"
+    val jna        = "5.10.0"
 
     // If you set this to None you can test with your locally installed version of Graal. Otherwise it will run in Docker
     // and build a Linux image (e.g. setting it to s"$graal-java11").
