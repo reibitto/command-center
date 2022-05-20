0f02bd3dbefffdab17ddf342a64d8ac34d82bc05
Scala Steward <me@scala-steward.org>
2022-05-20 04:17:03 +0200

    Update circe-core, circe-parser to 0.14.2

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object Build {
   object Version {
     val zio        = "1.0.13"
     val enumeratum = "1.7.0"
-    val circe      = "0.14.1"
+    val circe      = "0.14.2"
     val sttp       = "3.3.18"
     val graal      = "20.2.0"
     val swt        = "3.118.0"
