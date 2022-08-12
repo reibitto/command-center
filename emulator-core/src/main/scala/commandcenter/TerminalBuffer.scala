2741126a3555581cc7ca3067d5c40b4dc44e5bcf
Scala Steward <scala_steward@virtuslab.com>
2022-08-12 18:01:18 +0000

    Reformat with scalafmt 3.5.9
    Executed command: scalafmt --non-interactive

--- emulator-core/src/main/scala/commandcenter/TerminalBuffer.scala
+++ emulator-core/src/main/scala/commandcenter/TerminalBuffer.scala
@@ -3,6 +3,7 @@ package commandcenter
 import scala.collection.mutable
 
 class TerminalBuffer(val buffer: StringBuilder, val lineStartIndices: mutable.ArrayDeque[Int]) {
+
   def clear(): Unit = {
     buffer.clear()
     lineStartIndices.clear()
