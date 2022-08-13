fa7412b72da8f224dcf4d3f266adadb134e3e1e4
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-08-13 18:00:06 +0200

    Update scalafmt-core to 3.5.9 (#368)
    * Update scalafmt-core to 3.5.9
    
    * Reformat with scalafmt 3.5.9
    
    Executed command: scalafmt --non-interactive
    
    * Add 'Reformat with scalafmt 3.5.9' to .git-blame-ignore-revs

--- emulator-core/src/main/scala/commandcenter/GlobalActions.scala
+++ emulator-core/src/main/scala/commandcenter/GlobalActions.scala
@@ -1,69 +1,70 @@
 package commandcenter
 
-import commandcenter.CCRuntime.Env
 import commandcenter.shortcuts.Shortcuts
 import commandcenter.util.{CycleWindowState, WindowBounds, WindowManager}
+import commandcenter.CCRuntime.Env
 import zio.{RIO, Ref, ZIO}
 
 object GlobalActions {
+
   def setupCommon(actions: Vector[GlobalAction]): RIO[Env, Unit] =
     for {
       cycleWindowStateRef <- Ref.make(Option.empty[CycleWindowState])
-      _                   <- ZIO.foreachDiscard(actions) { action =>
-                               val run = action.id match {
-                                 case GlobalActionId.MinimizeWindow                      => WindowManager.minimizeWindow
-                                 case GlobalActionId.MaximizeWindow                      => WindowManager.maximizeWindow
-                                 case GlobalActionId.ToggleMaximizeWindow                => WindowManager.toggleMaximizeWindow
-                                 case GlobalActionId.CenterWindow                        => WindowManager.centerScreen
-                                 case GlobalActionId.MoveToPreviousScreen                => WindowManager.moveToPreviousDisplay
-                                 case GlobalActionId.MoveToNextScreen                    => WindowManager.moveToNextDisplay
-                                 case GlobalActionId.ResizeToScreenSize                  => WindowManager.resizeToScreenSize
-                                 case GlobalActionId.ResizeFullHeightMaintainAspectRatio =>
-                                   WindowManager.resizeFullHeightMaintainAspectRatio
+      _ <- ZIO.foreachDiscard(actions) { action =>
+             val run = action.id match {
+               case GlobalActionId.MinimizeWindow       => WindowManager.minimizeWindow
+               case GlobalActionId.MaximizeWindow       => WindowManager.maximizeWindow
+               case GlobalActionId.ToggleMaximizeWindow => WindowManager.toggleMaximizeWindow
+               case GlobalActionId.CenterWindow         => WindowManager.centerScreen
+               case GlobalActionId.MoveToPreviousScreen => WindowManager.moveToPreviousDisplay
+               case GlobalActionId.MoveToNextScreen     => WindowManager.moveToNextDisplay
+               case GlobalActionId.ResizeToScreenSize   => WindowManager.resizeToScreenSize
+               case GlobalActionId.ResizeFullHeightMaintainAspectRatio =>
+                 WindowManager.resizeFullHeightMaintainAspectRatio
 
-                                 case id @ GlobalActionId.CycleWindowSizeLeft =>
-                                   WindowManager
-                                     .cycleWindowSize(cycleWindowStateRef)(1, id.entryName)(
-                                       Vector(
-                                         WindowBounds(left = 0, top = 0, right = 0.5, bottom = 1.0),
-                                         WindowBounds(left = 0 / 3.0, top = 0, right = 2.0 / 3.0, bottom = 1.0),
-                                         WindowBounds(left = 0 / 3.0, top = 0, right = 1.0 / 3.0, bottom = 1.0)
-                                       )
-                                     )
+               case id @ GlobalActionId.CycleWindowSizeLeft =>
+                 WindowManager
+                   .cycleWindowSize(cycleWindowStateRef)(1, id.entryName)(
+                     Vector(
+                       WindowBounds(left = 0, top = 0, right = 0.5, bottom = 1.0),
+                       WindowBounds(left = 0 / 3.0, top = 0, right = 2.0 / 3.0, bottom = 1.0),
+                       WindowBounds(left = 0 / 3.0, top = 0, right = 1.0 / 3.0, bottom = 1.0)
+                     )
+                   )
 
-                                 case id @ GlobalActionId.CycleWindowSizeRight =>
-                                   WindowManager
-                                     .cycleWindowSize(cycleWindowStateRef)(1, id.entryName)(
-                                       Vector(
-                                         WindowBounds(left = 0.5, top = 0, right = 1.0, bottom = 1.0),
-                                         WindowBounds(left = 1.0 / 3.0, top = 0, right = 1.0, bottom = 1.0),
-                                         WindowBounds(left = 2.0 / 3.0, top = 0, right = 1.0, bottom = 1.0)
-                                       )
-                                     )
+               case id @ GlobalActionId.CycleWindowSizeRight =>
+                 WindowManager
+                   .cycleWindowSize(cycleWindowStateRef)(1, id.entryName)(
+                     Vector(
+                       WindowBounds(left = 0.5, top = 0, right = 1.0, bottom = 1.0),
+                       WindowBounds(left = 1.0 / 3.0, top = 0, right = 1.0, bottom = 1.0),
+                       WindowBounds(left = 2.0 / 3.0, top = 0, right = 1.0, bottom = 1.0)
+                     )
+                   )
 
-                                 case id @ GlobalActionId.CycleWindowSizeTop =>
-                                   WindowManager
-                                     .cycleWindowSize(cycleWindowStateRef)(1, id.entryName)(
-                                       Vector(
-                                         WindowBounds(left = 0, top = 0, right = 1.0, bottom = 0.5),
-                                         WindowBounds(left = 0, top = 0, right = 1.0, bottom = 2.0 / 3.0),
-                                         WindowBounds(left = 0, top = 0, right = 1.0, bottom = 1.0 / 3.0)
-                                       )
-                                     )
+               case id @ GlobalActionId.CycleWindowSizeTop =>
+                 WindowManager
+                   .cycleWindowSize(cycleWindowStateRef)(1, id.entryName)(
+                     Vector(
+                       WindowBounds(left = 0, top = 0, right = 1.0, bottom = 0.5),
+                       WindowBounds(left = 0, top = 0, right = 1.0, bottom = 2.0 / 3.0),
+                       WindowBounds(left = 0, top = 0, right = 1.0, bottom = 1.0 / 3.0)
+                     )
+                   )
 
-                                 case id @ GlobalActionId.CycleWindowSizeBottom =>
-                                   WindowManager
-                                     .cycleWindowSize(cycleWindowStateRef)(1, id.entryName)(
-                                       Vector(
-                                         WindowBounds(left = 0, top = 0.5, right = 1.0, bottom = 1.0),
-                                         WindowBounds(left = 0, top = 1.0 / 3.0, right = 1.0, bottom = 1.0),
-                                         WindowBounds(left = 0, top = 2.0 / 3.0, right = 1.0, bottom = 1.0)
-                                       )
-                                     )
-                               }
+               case id @ GlobalActionId.CycleWindowSizeBottom =>
+                 WindowManager
+                   .cycleWindowSize(cycleWindowStateRef)(1, id.entryName)(
+                     Vector(
+                       WindowBounds(left = 0, top = 0.5, right = 1.0, bottom = 1.0),
+                       WindowBounds(left = 0, top = 1.0 / 3.0, right = 1.0, bottom = 1.0),
+                       WindowBounds(left = 0, top = 2.0 / 3.0, right = 1.0, bottom = 1.0)
+                     )
+                   )
+             }
 
-                               Shortcuts.addGlobalShortcut(action.shortcut)(_ => run.ignore)
-                             }
+             Shortcuts.addGlobalShortcut(action.shortcut)(_ => run.ignore)
+           }
     } yield ()
 
 }
