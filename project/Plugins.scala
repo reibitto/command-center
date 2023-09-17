1bb93d4620fc94b6857895acb3d1e3ab7935f56b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-09-17 05:56:35 +0200

    Update zio-clippy to 0.0.4 (#492)

--- project/Plugins.scala
+++ project/Plugins.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Plugins {
 
   lazy val BaseCompilerPlugins = Seq(
-    compilerPlugin("com.hmemcpy" %% "zio-clippy" % "0.0.2"),
+    compilerPlugin("com.hmemcpy" %% "zio-clippy" % "0.0.4"),
     compilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1"),
     compilerPlugin("org.typelevel" %% "kind-projector" % "0.13.2" cross CrossVersion.full)
   )
