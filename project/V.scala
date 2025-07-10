1a1153c04d86b393e47fe3c6cfb0bf1bda9d8cfb
reibitto <reibitto@users.noreply.github.com>
2025-07-11 02:14:56 +0900

    Revert swt version

--- project/V.scala
+++ project/V.scala
@@ -37,7 +37,9 @@ object V {
 
   val sttp = "3.11.0"
 
-  val swt = "3.130.0"
+  // For some reason 3.130.0 causes "Libraries for platform win32 cannot be loaded because of incompatible environment"
+  // TODO: Might be an issue with sbt-assembly config. Try to fix later.
+  val swt = "3.129.0"
 
   val zio = "2.1.19"
 
