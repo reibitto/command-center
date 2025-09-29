f710310335f0ce18471c748ea0424cf6e20b1adc
reibitto <reibitto@users.noreply.github.com>
2025-09-29 15:58:50 +0900

    Fix SWT setup

--- project/V.scala
+++ project/V.scala
@@ -37,8 +37,6 @@ object V {
 
   val sttp = "3.11.0"
 
-  // For some reason 3.130.0 causes "Libraries for platform win32 cannot be loaded because of incompatible environment"
-  // TODO: Might be an issue with sbt-assembly config. Try to fix later.
   val swt = "3.131.0"
 
   val zio = "2.1.21"
